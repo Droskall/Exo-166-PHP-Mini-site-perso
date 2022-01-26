f1494c8e0dfa751055263f530274c1ee13592d5c
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2022-01-26 16:19:39 +0100

    fini

--- pages/home.php
+++ pages/home.php
@@ -3,11 +3,11 @@
     <p>Bonjour et bienvenu(e) sur mon bestiaire.</p>
     <p>Voici quelque images des monstres que j'ai rencontrer:</p>
     <div id="containerImg">
-        <img src="../img/monster1.png" alt="" width="300" height="200">
-        <img src="../img/monster2.png" alt="" width="300" height="200">
-        <img src="../img/monster3.png" alt="" width="300" height="200">
-        <img src="../img/monster4.png" alt="" width="300" height="200">
-        <img src="../img/monster5.png" alt="" width="300" height="200">
-        <img src="../img/monster6.png" alt="" width="300" height="200">
+        <img src="../img/monster1.png" alt="" class="monster">
+        <img src="../img/monster2.png" alt="" class="monster">
+        <img src="../img/monster3.png" alt="" class="monster">
+        <img src="../img/monster4.png" alt="" class="monster">
+        <img src="../img/monster5.png" alt="" class="monster">
+        <img src="../img/monster6.png" alt="" class="monster">
     </div>
 </section>
